f30fe60646d917e5e5620d48566aef02ae7b6990
Max Hansen <splautmail@gmail.com>
2019-03-28 19:39:40 +0100

    This is the initial mongodb setup, next up is setting up the configuration

--- server/app.js
+++ server/app.js
@@ -1,4 +1,19 @@
 import express from 'express'
+import mongoose, { mongo } from 'mongoose' 
+
+const mongooseConnectionPromise = mongoose.connect('mongodb://127.0.0.1:27017');
+
+var db = mongoose.connection;
+db.on('error', function(err){
+    console.error(`MongoDB connection error: ${err}`);
+    process.exit(-1);
+});
+
+db.once('open', function callback(){
+    console.log('connected!');
+});
+
+
 
 var server = express()
     .get('/', (reg, res) => {
